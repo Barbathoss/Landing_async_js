d23bdd278ba76813743ca5b3ac877060ef0b2ded
Alex Quispe Torres <alexqtorre@gmail.com>
2025-02-26 20:29:48 -0500

    Updates

---
+++ assets/main.js
@@ -0,0 +1,50 @@
+
+
+const API = 'https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PL06XWdnMHRc7AM6b6u95Ro1ApNwQ2BC-y&part=snippet';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '5110b14c1amshc44b6a0f8f96730p12a8e6jsn5a9578dd985e',
+		'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi){
+    
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+   
+}
+
+
+
+(async()=>{
+    
+
+try {
+	const videos = await fetchData(API);
+	let view = ` ${videos.items.map(video => `
+         <div class="group relative">
+          <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+                ${video.snippet.title}
+            </h3>
+          </div>
+        </div>
+    `).slice(0,5).join('') }`;
+    content.innerHTML = view;
+
+    } catch (error) {
+        console.error(error);
+    }
+    })();
+
